21aa3466ff28b0a0400d7fe8568e3a16fe1248d9
youqing <youqing89816@163.com>
2016-04-19 15:25:26 +0800

    delete file1

--- proj.android/assets/config.lua
+++
@@ -1,1 +0,0 @@
-scriptsVersion = 1013
--- proj.android/obj/local/armeabi/objs/cocos2dx_static/ccFPSImages.o.d
+++
@@ -1,5 +0,0 @@
-obj/local/armeabi/objs/cocos2dx_static/ccFPSImages.o: \
- /Users/likai/Documents/client/cocos2dx/cocos2dx/ccFPSImages.c \
- /Users/likai/Documents/client/cocos2dx/cocos2dx/ccFPSImages.h
-
-/Users/likai/Documents/client/cocos2dx/cocos2dx/ccFPSImages.h:
