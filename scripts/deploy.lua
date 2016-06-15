1dcc18ec61ca8185c0807abbc8e52792eec3f32c
litian <125846662@qq.com>
2016-06-15 16:33:53 +0800

    20160615 16:33

--- scripts/deploy.lua
+++ scripts/deploy.lua
@@ -2,11 +2,12 @@ IP = "10.0.37.11"
 --IP = "10.0.37.45"
 --IP="127.0.0.1"
 PORT = 4000
-isImageEncoded = 1
+isImageEncoded = 0
 splitURLChar = '/'
 -- updateDownLoadURL = "http://fruit.youngdream.cc:9237/cashout/"  -- 测试服
 -- updateDownLoadURL = "http://115.236.35.202:9237/cashout/"
-updateDownLoadURL = "http://120.27.156.196:8080/"
+-- updateDownLoadURL = "http://120.27.156.196:8080/"--ip
+updateDownLoadURL = "http://xalyyy.com:8080/"
 
 -- updateDownLoadURL = "http://218.66.170.6:8081/cash/"  -- 正式服
 loginPayServerUrl = "http://218.66.170.6:8089" --sdk登陆验证新服
